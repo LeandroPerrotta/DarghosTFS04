dc1e68a06f50a0d49d26943239a5662d6ca02edc
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-02-17 21:38:16 +0000

    * fix potions
    git-svn-id: svn://darghos.com.br/darghos/tfs@1980 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- data/trunk/actions/scripts/liquids/potions.lua
+++ data/trunk/actions/scripts/liquids/potions.lua
@@ -16,17 +16,17 @@ config.splashable = getBooleanFromString(config.splashable)
 config.realAnimation = getBooleanFromString(config.realAnimation)
 
 local POTIONS = {
-	[8704] = {empty = 7636, splash = 42, health = {50, 100}}, -- small health potion
+	[8704] = {empty = 7636, splash = 42, health = {60, 90}}, -- small health potion
 	[7618] = {empty = 7636, splash = 42, health = {100, 200}}, -- health potion
-	[7588] = {empty = 7634, splash = 42, health = {200, 400}, level = 50, vocations = {3, 4, 7, 8}, vocStr = "knights and paladins"}, -- strong health potion
-	[7591] = {empty = 7635, splash = 42, health = {500, 700}, level = 80, vocations = {4, 8}, vocStr = "knights"}, -- great health potion
-	[8473] = {empty = 7635, splash = 42, health = {800, 1000}, level = 130, vocations = {4, 8}, vocStr = "knights"}, -- ultimate health potion
+	[7588] = {empty = 7634, splash = 42, health = {250, 350}, level = 50, vocations = {3, 4, 7, 8}, vocStr = "knights and paladins"}, -- strong health potion
+	[7591] = {empty = 7635, splash = 42, health = {450, 650}, level = 80, vocations = {4, 8}, vocStr = "knights"}, -- great health potion
+	[8473] = {empty = 7635, splash = 42, health = {700, 850}, level = 130, vocations = {4, 8}, vocStr = "knights"}, -- ultimate health potion
 
-	[7620] = {empty = 7636, splash = 47, mana = {70, 130}}, -- mana potion
-	[7589] = {empty = 7634, splash = 47, mana = {110, 190}, level = 50, vocations = {1, 2, 3, 5, 6, 7}, vocStr = "sorcerers, druids and paladins"}, -- strong mana potion
-	[7590] = {empty = 7635, splash = 47, mana = {200, 300}, level = 80, vocations = {1, 2, 5, 6}, vocStr = "sorcerers and druids"}, -- great mana potion
+	[7620] = {empty = 7636, splash = 47, mana = {75, 125}}, -- mana potion
+	[7589] = {empty = 7634, splash = 47, mana = {115, 185}, level = 50, vocations = {1, 2, 3, 5, 6, 7}, vocStr = "sorcerers, druids and paladins"}, -- strong mana potion
+	[7590] = {empty = 7635, splash = 47, mana = {150, 250}, level = 80, vocations = {1, 2, 5, 6}, vocStr = "sorcerers and druids"}, -- great mana potion
 
-	[8472] = {empty = 7635, splash = 43, health = {200, 400}, mana = {110, 190}, level = 80, vocations = {3, 7}, vocStr = "paladins"} -- great spirit potion
+	[8472] = {empty = 7635, splash = 43, health = {250, 350}, mana = {110, 190}, level = 80, vocations = {3, 7}, vocStr = "paladins"} -- great spirit potion
 }
 
 local exhaust = createConditionObject(CONDITION_EXHAUST)
