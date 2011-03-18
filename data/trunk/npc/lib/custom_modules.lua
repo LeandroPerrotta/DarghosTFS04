49cf10d09ed4428080407250f941d9e5bba2f3ee
Leandro Perrotta <leandro_perrotta@hotmail.com>
2011-03-18 15:48:34 +0000

    * Revisado resapwns de Hero em Quendor * Corrigido entrada para "area perigosa" do DLair de Quendor * Implementado metodos para tratamento de troca de itens de Addons para o NPC Jiddo System. * NPCs de artigos magicos movidos do sistema Simone para o sistema Jiddo.
    git-svn-id: svn://darghos.com.br/darghos/tfs@2040 5021ead5-42e4-5646-a3ec-22fc1710fdc7

--- data/trunk/npc/lib/custom_modules.lua
+++ data/trunk/npc/lib/custom_modules.lua
@@ -2,6 +2,100 @@
 
 D_CustomNpcModules = {}
 
+function D_CustomNpcModules.addonTradeItems(cid, message, keywords, parameters, node)
+
+	local npcHandler = parameters.npcHandler
+	
+	if(npcHandler == nil) then
+		print('[Warning - ' .. getCreatureName(getNpcId()) .. '] NpcSystem:', 'StdModule.travel - Call without any npcHandler instance.')
+		return false
+	end
+
+	if(not npcHandler:isFocused(cid)) then
+		return false
+	end
+
+	local foundAll = true
+
+	local itemsToRemove = {}
+
+	for _,item in pairs(parameters.neededItems) do
+	
+		if(item.anyOf ~= nil) then
+		
+			local found = false
+		
+			for _,sub in pairs(item.anyOf) do
+			
+				local count = (sub.count ~= nil) and sub.count or item.count 
+			
+				if(getPlayerItemCount(cid, sub.id) >= count) then
+					found = true
+					table.insert(itemsToRemove, {id = sub.id, count = count})
+					break
+				end			
+			end
+			
+			if(not found) then
+				foundAll = false
+				break
+			end		
+		else
+			if(getPlayerItemCount(cid, item.id) >= item.count) then
+				table.insert(itemsToRemove, {id = sub.id, count = item.count})
+			else
+				foundAll = false
+				break
+			end			
+		end
+	end
+	
+	if(not foundAll) then		
+		local msg = (parameters.fail ~= nil) and parameters.fail or "Sorry but you not have all needed items..."
+		npcHandler:say(msg, cid)
+		npcHandler:resetNpc()
+		return true
+	end
+	
+	local neededCap = 0
+	
+	for _,item in pairs(parameters.receiveItems) do
+	
+		neededCap = neededCap + getItemWeightById(item.id, item.count)
+	end		
+	
+	if(getPlayerFreeCap(cid) < neededCap) then
+		npcHandler:say("You do not have enough capacity for all items.", cid)
+		npcHandler:resetNpc()
+		return true	
+	end
+	
+	for k,v in pairs(itemsToRemove) do
+		if(not doPlayerRemoveItem(cid, v.id, v.count)) then
+			print('[Warning - ' .. getCreatureName(getNpcId()) .. '] NpcSystem:', 'D_CustomNpcModules.addonTradeItems - Impossible to remove an previously checked item, aborted. Details:', '{player=' .. getCreatureName(cid) .. ', item_id=' .. v.id .. ', count=' .. v.count .. '}', 'Added items: ' .. table.show(addedItems))
+			return false
+		end
+	end	
+	
+	local addedItems = {}
+	
+	for _,item in pairs(parameters.receiveItems) do
+	
+		local tmp = doCreateItemEx(item.id, item.count)
+		if(doPlayerAddItemEx(cid, tmp, true) ~= RETURNVALUE_NOERROR) then
+			print('[Warning - ' .. getCreatureName(getNpcId()) .. '] NpcSystem:', 'D_CustomNpcModules.addonTradeItems - Impossible to give an item, aborted. Details:', '{player=' .. getCreatureName(cid) .. ', item_id=' .. v.id .. ', count=' .. v.count .. '}', 'Added items: ' .. table.show(addedItems))
+			return false
+		else
+			table.insert(addedItems, {id = item.id, count = item.count})
+		end
+	end		
+	
+	local msg = (parameters.success ~= nil) and parameters.success or "Thanks! Here it is! I hope you are happy!"
+	npcHandler:say(msg, cid)
+	npcHandler:resetNpc()
+	return true	
+end
+
 function D_CustomNpcModules.travelTrainingIsland(cid, message, keywords, parameters, node)
 	local npcHandler = parameters.npcHandler
 	if(npcHandler == nil) then
--- data/trunk/npc/lib/npc.lua
+++ data/trunk/npc/lib/npc.lua
@@ -4,6 +4,7 @@ dofile(getDataDir() .. 'npc/lib/npc_dialog.lua')
 dofile(getDataDir() .. 'npc/lib/npc_tasks.lua')
 dofile(getDataDir() .. 'npc/lib/npc_system.lua')
 dofile(getDataDir() .. 'npc/lib/custom_modules.lua')
+dofile(getDataDir() .. 'npc/lib/addonItems.lua')
 
 function selfSayChannel(cid, message)
 	return selfSay(message, cid, false)
